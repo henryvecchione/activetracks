d6befcd00f86e9c11b011b62daa0a459023b5720
Henry Vecchione <68787777+henryvecchione@users.noreply.github.com>
2021-10-29 10:28:10 -0400

    method comments

--- stravify.py
+++ stravify.py
@@ -44,13 +44,12 @@ SPOTIPY_REDIRECT_URI = os.environ['SPOTIPY_REDIRECT_URI']
 SCOPE = os.environ['SCOPE']
 CACHE = os.environ['CACHE']
 
+sp_oauth = oauth2.SpotifyOAuth( SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI,scope=SCOPE, cache_path=CACHE )
 
-# @app.route('/', methods=['GET'])
-# def index():
-#     html = render_template('index.html')
-#     response = make_response(html)
-#     return response
 
+# -------------------------------------------------------------------- #
+# take the start time of Spotify song, the duration, and return the start
+# and end as datetime objects
 def getEnd (startString, durationMS):
     startDate = dateutil.parser.parse(startString)
     duration = timedelta(milliseconds=durationMS)
@@ -103,14 +102,15 @@ def webhookPOST():
     # it isnt an activity (eg a post).
     if info['aspect_type'] == 'delete': return make_response(info)
     if info['object_type'] != 'activity': return make_response(info)
+    
     activityID = info['object_id']
     userID = info['owner_id']
 
     # get the activity 
     activity = getActivityByID(userID, activityID)
     if activity: # if it's a valid one
-        if activity['manual']: return make_response(info) # if manual the interval is irrelevant
-        print('Activity: \n' , activity['name'])
+        if activity['manual']: return make_response(info) # if manual the time might not be accurate, so do nothing
+        print('Activity: \n' , activity['name']) # console log for testing
         # get the interval 
         start, end = getEnd(activity['start_date'], (activity['elapsed_time']*1000))
         # get the songs in that interval
@@ -156,9 +156,6 @@ def about():
     html = render_template('about.html')
     return make_response(html)
 
-    
-sp_oauth = oauth2.SpotifyOAuth( SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI,scope=SCOPE, cache_path=CACHE )
-
 def htmlForLoginButton():
     auth_url = getSPOauthURI()
     htmlLoginButton =  auth_url
@@ -172,12 +169,10 @@ def getSPOauthURI():
 def spotifyLogin():
     html = render_template('spotifyLogin.html', loginURL=htmlForLoginButton())
     return make_response(html)
+
 # -------------------------------------------------------------------- #
-
-
-
-
-
+# route for authenticating spotify account 
+# significant help came from https://github.com/perelin/spotipy_oauth_demo/blob/master/spotipy_oauth_demo.py
 
 @app.route("/spotifyAuth", methods=['GET'])
 def spotifyAuth():
@@ -225,13 +220,14 @@ def spotifyAuth():
         results = requests.get(rp_url , headers=header)
         userID = user.json()['id']
 
+        # if the user is not in the database, add them to it
         if db.getUserInfoSpotify(userID) is None:
-
             db.addNewUser('-1', '-1', '-1', access_token, refresh_token, userID)
+        # if they are, update their row with the new tokens
         else:
-
             db.updateUserSpotify(userID, access_token, refresh_token)
-
+        
+        # get the tracks they listened to 
         tracksList = []
         for track in results.json()['items']:
             artist = track['track']['artists'][0]['name']
@@ -263,16 +259,14 @@ def spotifyAuth():
 
 
 # -------------------------------------------------------------------- #
-
+# Route for authenticating Strava account
 @app.route('/stravaAuthed', methods=['GET'])
 def stravaAuthed():
     code = request.args.get('code')
 
     access_token = client.exchange_code_for_token(client_id=STRAVA_CLIENT_ID,
                                                     client_secret=STRAVA_CLIENT_SECRET, code=code)
-    # Now store that access token somewhere (for now, it's just a local variable)
 
-    print(access_token, '*****')
     client.access_token = access_token
 
     auth_url = "https://www.strava.com/oauth/token"
@@ -286,15 +280,15 @@ def stravaAuthed():
         'grant_type': "refresh_token",
         'f': 'json'
     }
-
+    # get the access and refresh tokens
     print("Requesting Token...\n")
     res = requests.post(auth_url, data=payload, verify=False)
     access_token = res.json()['access_token']
     refresh_token = res.json()['refresh_token']
-    print("Access Token = {}\n".format(access_token))
-
     header = {'Authorization': 'Bearer ' + access_token}
     param = {'per_page': 50, 'page': 1}
+    
+    # get the activities
     activities_data = requests.get(activites_url, headers=header, params=param).json()
     res = requests.get(athlete_url, headers=header).json()
     athleteID = res['id']
@@ -302,6 +296,7 @@ def stravaAuthed():
 
     spotify_user = session['spotify_user']
 
+    # if they're new to the DB, add them by the key of the spotify user logged in previously
     if db.getUserInfoStrava(athleteID) is None:
         db.updateUserStravaBySpotify(spotify_user, athleteID, access_token, refresh_token)
     else:
@@ -312,6 +307,8 @@ def stravaAuthed():
         tracksList = session['recentlyPlayed']
     else:
         print('NO TRACKLIST')
+        
+    # get their recent activities 
     activitiesList = []
     for activity in activities_data:
         name = activity['name']
@@ -323,6 +320,7 @@ def stravaAuthed():
         acDict = {'name' : name, 'idNo' : idNo, 'kind' : kind, 'st' :st, 'et' : et}
         activitiesList.append(acDict)
 
+    # compare the track list with the activity list to get songs played during the activities
     concurrence = checkConcurrence(tracksList, activitiesList)
 
     if not concurrence:
@@ -333,6 +331,8 @@ def stravaAuthed():
         concurrence = stravaMethods.getSongIndices(concurrence)
 
 
+    # code for more complex data analysis, future feature
+    
     # f = open('streams.txt', 'w')
 
     # for activity in concurrence:
@@ -353,7 +353,7 @@ def stravaAuthed():
     return response
 
 # -------------------------------------------------------------------- #
-
+# edits the description of a strava activity
 def editActivity(userID, activityID, newDescription): 
     activity_url = 'https://www.strava.com/api/v3/activities/'
     activity_url = activity_url + str(activityID)
@@ -366,14 +366,14 @@ def editActivity(userID, activityID, newDescription):
         'grant_type': "refresh_token",
         'refresh_token': refresh_token,
     }
+    # authenticate the user 
     res = requests.post(auth_url, data=payload)
-
     access_token = res.json()['access_token']
     refresh_token = res.json()['refresh_token']
     header = {'Authorization': 'Bearer ' + access_token}
-
     db.updateUserStrava(userID, access_token, refresh_token)
 
+    # get the old description: we are adding to it rather than changing wholesale
     oldDescription = requests.get(activity_url, headers=header).json()['description']
     if oldDescription == None: oldDescription = ''
     else: oldDescription = oldDescription +'\n'
@@ -381,6 +381,7 @@ def editActivity(userID, activityID, newDescription):
         updatedActivity = {
             'description':  oldDescription + newDescription,
         }
+        # make the PUT to update the activity with the new info
         updateActivity = requests.put(activity_url, headers=header, data=updatedActivity, verify=False)
         return updateActivity
     else: 
@@ -393,16 +394,18 @@ def songsForDesc(strava_user, activity_start, activity_end):
         'grant_type' : 'refresh_token',
         'refresh_token' : refresh_token
     }
-
+    # authenticate spotify user
     authStr = SPOTIPY_CLIENT_ID +':'+ SPOTIPY_CLIENT_SECRET
     authStrB64 = base64.urlsafe_b64encode(authStr.encode()).decode()
     header = {'Authorization': 'Basic ' + authStrB64}
     res = requests.post(auth_url, data=payload, headers=header)
     access_token = res.json()['access_token']
 
+    # update database information 
     spotify_user = db.getUserInfoStrava(strava_user)[5]
     db.updateUserSpotify(spotify_user, access_token, refresh_token)
-
+    
+    # get the recently played songs 
     sp = spotipy.Spotify(access_token)
     userID = sp.current_user()['id']
     results = sp.current_user_recently_played(limit=50)
@@ -423,9 +426,11 @@ def songsForDesc(strava_user, activity_start, activity_end):
 
     activity_start = activity_start.replace(tzinfo=None)
     activity_end = activity_end.replace(tzinfo=None)
+    # return the songs played between activity_start and activity_end
     songs = checkConcurrenceSingle(tracksList, activity_start, activity_end)
     return songs
 
+# test code
 if __name__ == "__main__":
     userID = 2987820
     activityID = 6097750589
